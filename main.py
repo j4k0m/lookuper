ab347acaa0083b37543e3d16643f104188be5147
Jakom <48088579+RyouYoo@users.noreply.github.com>
2021-04-25 13:59:58 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 #!/usr/bin/python3
 import requests, json, sys
+from multiprocessing import Pool
 
-URL = "https://ipwhois.app/json/{}"
 
-for i in open(sys.argv[1], "r").readlines():
+def check(i):
     i = i.strip("\n")
     _i = i.split("/")[0]
     json_data = json.loads(requests.get(URL.format(_i)).text)
     with open("./result.txt", "a+") as result:
         fr = f"{i}, {json_data['country']}, {json_data['city']}"
-        print(fr)
         result.write(fr + "\n")
         result.close()
+        print(fr)
+
+URL = "https://ipwhois.app/json/{}"
+
+if __name__ == '__main__':
+    with Pool(50) as c:
+        c.map(check, open(sys.argv[1], "r").readlines())
